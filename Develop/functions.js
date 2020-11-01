da2e345dd802d34ec9929391d6de2478af4e1d9d
Sean Hopkins <seahopki12@gmail.com>
2020-11-01 18:14:34 -0500

    Created skeleton for function defintions

--- Develop/functions.js
+++ Develop/functions.js
@@ -1,13 +1,17 @@
 const inquirer = require("inquirer");
 
-inquirer
-    .prompt([
-        {
-            name: "task",
-            type: "list",
-            message: "What would you like to do?",
-            choices: ["View All Employees", "View All Employees By Department", "View All Employees By Role", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
-        }
-    ]).then(function(response){
-        console.log(response.task);
-    })
+class tasks {
+
+    // constructor(task) {
+    //     this.task = task;
+    // }
+
+    allEmployees() {};
+    employeesByDept() {};
+    employeesByMgr() {};
+    addEmployee() {};
+    removeEmployee() {};
+    updateDept() {};
+    updateMgr() {};
+}
+
--- index.js
+++ index.js
@@ -19,3 +19,18 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
     afterConnection();
 });
+
+function afterConnection() {
+    inquirer
+        .prompt([
+            {
+                name: "task",
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["View All Employees", "View All Employees By Department", "View All Employees By Role", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
+            }
+        ]).then(function (response) {
+            console.log(response.task);
+            
+        });
+}
